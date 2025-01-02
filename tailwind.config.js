700a0935be508159034c0aafe87f638dcda10d3e
Gladoncio <42326122+gladoncio@users.noreply.github.com>
2025-01-02 18:35:22 -0300

    update portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,17 @@ module.exports = {
     './public/**/*.html',             // Incluye HTML en la carpeta public
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blink: 'blink 1s steps(2, start) infinite', // Agrega la animación de parpadeo
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: 1 }, // El cursor está visible al inicio y al final
+          '50%': { opacity: 0 },      // El cursor se oculta en la mitad del ciclo
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
